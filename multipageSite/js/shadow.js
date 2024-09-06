fba9a402ffa329275a249e0add37964c93c1c18f
andreavaughan <andrea.vaughan@pendo.io>
2024-09-06 13:58:47 -0400

    create basic shadow node

--- multipageSite/js/shadow.js
+++ multipageSite/js/shadow.js
@@ -1,7 +1,14 @@
 function attachShadow() {
     let shadow = document.getElementById('attachShadow');
-    shadow.attachShadow({ mode: "open"});
-    shadow.innerHTML = `<p>Boo!</p>`;
+    let container = shadow.attachShadow({ mode: "open"});
+
+    let b1 = document.createElement('button');
+    b1.setAttribute("id", "1");
+    b1.setAttribute("type", "button");
+    b1.setAttribute("style", "height: 50px; color: #fff; background-color: rgba(222, 40, 100, 1); border-color: rgba(222, 40, 100, 1);");
+    b1.innerHTML = "Button"
+
+    container.appendChild(b1);
 };
 
 attachShadow()
