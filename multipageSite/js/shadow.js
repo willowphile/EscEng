8630f082f73d2efafd83f797b9bc308098d0e51f
andreavaughan <andrea.vaughan@pendo.io>
2024-09-06 17:04:29 -0400

    basic page outline created

--- multipageSite/js/shadow.js
+++ multipageSite/js/shadow.js
@@ -1,3 +1,21 @@
+const sheet = new CSSStyleSheet();
+const css =`
+    button {
+        height: 50px;
+        color: #fff;
+        padding-right: 1rem;
+        padding-left: 1rem; 
+        background-color: rgba(222, 40, 100, 1);
+        border-radius: 12px;
+        border: none;
+        margin: 20px;
+    }
+    
+
+`
+sheet.replaceSync(css);
+
+
 function attachShadow() {
     let shadow = document.getElementById('attachShadow');
     let container = shadow.attachShadow({ mode: "open"});
@@ -5,10 +23,12 @@ function attachShadow() {
     let b1 = document.createElement('button');
     b1.setAttribute("id", "1");
     b1.setAttribute("type", "button");
-    b1.setAttribute("style", "height: 50px; color: #fff; background-color: rgba(222, 40, 100, 1); border-color: rgba(222, 40, 100, 1);");
     b1.innerHTML = "Button"
 
+    container.adoptedStyleSheets = [sheet]
     container.appendChild(b1);
 };
+
+
 
 attachShadow()
